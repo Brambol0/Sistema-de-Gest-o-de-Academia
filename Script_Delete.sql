ee230f9d04e211f29f970790d63a137a9cfc0633
Erick Luiz Brambila Batista <brambilabatista@gmail.com>
2025-11-26 16:32:47 -0300

    Update Script_Delete.sql

--- Script_Delete.sql
+++ Script_Delete.sql
@@ -1,8 +1,6 @@
-DELETE FROM avaliacao
+DELETE FROM resultado_cliente WHERE id_cliente = 3;
+
+DELETE FROM avaliacao 
 WHERE id_cliente = 2 AND id_instrutor = 2;
 
-DELETE FROM treino
-WHERE id_plano = 3;
-
-DELETE FROM aluno
-WHERE id_cliente = 3;
+DELETE FROM treino WHERE id_treino = 3;
