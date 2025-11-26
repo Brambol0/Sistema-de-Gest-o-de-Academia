f8af52616fc6a091080e7c333b24e61fcd2f2f64
Erick Luiz Brambila Batista <brambilabatista@gmail.com>
2025-11-26 16:32:11 -0300

    Update Script_Select.sql

--- Script_Select.sql
+++ Script_Select.sql
@@ -1,23 +1,20 @@
--- 1. Listar todos os alunos e seus planos
-SELECT a.nome, p.mensal
+-- 1. Selecionar nome e telefone dos alunos
+SELECT nome, telefone FROM aluno;
+
+-- 2. Alunos e seus planos
+SELECT a.nome, p.mensal 
 FROM aluno a
 JOIN plano p ON p.id_cliente = a.id_cliente;
 
--- 2. Buscar avaliações com instrutor e cliente
+-- 3. Avaliações com instrutores
 SELECT a.nome AS aluno, i.nome AS instrutor, av.data_avaliacao
 FROM avaliacao av
 JOIN aluno a ON a.id_cliente = av.id_cliente
-JOIN instrutor i ON i.id_instrutor = av.id_instrutor;
+JOIN instrutor i ON i.id_instrutor = av.id_instrutor
+ORDER BY av.data_avaliacao DESC;
 
--- 3. Treinos de um cliente específico
-SELECT a.nome, t.treino1, t.treino2, t.treino3
-FROM treino t
-JOIN plano p ON p.id_plano = t.id_plano
-JOIN aluno a ON a.id_cliente = p.id_cliente
-WHERE a.id_cliente = 1;
+-- 4. Limitar treinos
+SELECT * FROM treino LIMIT 2;
 
--- 4. Ordem dos clientes por idade
-SELECT * FROM aluno ORDER BY idade DESC;
-
--- 5. Limitar resultados
-SELECT nome, telefone FROM aluno LIMIT 2;
+-- 5. Selecionar alunos com perda de peso específica
+SELECT * FROM resultado_cliente WHERE perda_de_peso = '2kg';
