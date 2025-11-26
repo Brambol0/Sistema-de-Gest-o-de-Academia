4a39fb6266835d41d6e8d7fe891521bc8dc0456f
Erick Luiz Brambila Batista <brambilabatista@gmail.com>
2025-11-26 16:32:33 -0300

    Update and rename Script_Update to Script_Update.sql

---
+++ Script_Update.sql
@@ -0,0 +1,11 @@
+UPDATE aluno 
+SET telefone = '11912345678' 
+WHERE id_cliente = 1;
+
+UPDATE plano 
+SET mensal = 'M1-Atualizado' 
+WHERE id_plano = 1;
+
+UPDATE instrutor 
+SET especialidade = 'Treinamento Funcional' 
+WHERE id_instrutor = 2;
